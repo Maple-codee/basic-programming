37451256ac48cdbea27b75cc45d155bc94d32000
Maple-codee <overlord747911@gmail.com>
2024-07-19 23:04:48 +0700

    เปรียบเทียบค่า

--- ep1/index.ts
+++ ep1/index.ts
@@ -1,18 +1,18 @@
-const firstname: string = 'ณฐนนท นิโลตบล'
+const firstname: string = 'ณฐนนท นิโลตบล' // กำหนดค่าคงที่ให้ตัวแปรstringคือชื่อจริง
 
-let y: number = 10
-y = 20
+let y: number = 10 // แทนค่าy=10
+y = 20 // เปลี่ยนค่าตัวแปรyเป็น20
 
-let ต้องการกลับบ้าน: boolean = true
-console.log(firstname)
-console.log(y)
-console.log(ต้องการกลับบ้าน)
+let ต้องการกลับบ้าน: boolean = true // กำหนดตัวแปร
+console.log(firstname) // บอกชื่อจริง
+console.log(y) // บอกตัวแปรy
+console.log(ต้องการกลับบ้าน) // บอกตัวแปร 
 
-const a: number = 8
-const b: number = 69
+const a: number = 8 // กำหนดค่าคงที่aคือ8
+const b: number = 69 // กำหนดค่าคงที่bคือ69
 
-console.log(a === b)
+console.log(a === b) // แสดงผลว่าaเท่ากับbหรือไม่
 
-console.log(a > b)
+console.log(a > b) // แสดงผลว่าaมากกว่าbหรือไม่
 
-console.log(a <= b)
+console.log(a <= b) // แสดงผลว่าaน้อยกว่าหรือเท่ากับbหรือไม่
