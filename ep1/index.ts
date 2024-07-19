be6856c20360568549f7c4e76871607a5b509800
Maple-codee <overlord747911@gmail.com>
2024-07-19 16:08:21 +0700

    เปรียบเทียบค่า

--- ep1/index.ts
+++ ep1/index.ts
@@ -7,3 +7,12 @@ let ต้องการกลับบ้าน: boolean = true
 console.log(firstname)
 console.log(y)
 console.log(ต้องการกลับบ้าน)
+
+const a: number = 8
+const b: number = 69
+
+console.log (a === b)
+
+console.log (a > b)
+
+console.log (a <= b)
