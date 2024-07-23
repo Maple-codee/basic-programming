7f4e33871d88a8cce9670d0358b56ad1a0f3c074
Penpitcha03 <645051200287@mail.rmutk.ac.th.com>
2024-07-23 16:06:30 +0700

    เรียนfunction

--- ep1/index.ts
+++ ep1/index.ts
@@ -8,6 +8,8 @@ console.log(firstname) // บอกชื่อจริง
 console.log(y) // บอกตัวแปรy
 console.log(ต้องการกลับบ้าน) // บอกตัวแปร 
 
+
+
 const a: number = 8 // กำหนดค่าคงที่aคือ8
 const b: number = 69 // กำหนดค่าคงที่bคือ69
 
@@ -16,3 +18,50 @@ console.log(a === b) // แสดงผลว่าaเท่ากับbหร
 console.log(a > b) // แสดงผลว่าaมากกว่าbหรือไม่
 
 console.log(a <= b) // แสดงผลว่าaน้อยกว่าหรือเท่ากับbหรือไม่
+
+
+
+function boug(a: number, b: number) {
+    return a + b
+}
+
+console.log(boug(2, 3))
+
+
+
+function lob(a: number, b: number) {
+    return a - b
+}
+
+console.log(lob(5, 2))
+
+
+
+function kun(a: number, b: number, c: number, d: number) {
+    return a * b * c * d
+}
+
+console.log(kun(2, 2, 2, 2))
+
+
+
+function grade(homework: number, midterm: number, final: number) {
+    let score: number = homework + midterm + final
+
+    if (score < 50) {
+        return 'F'
+    } else if (score < 60) {
+        return 'D'
+    } else if (score < 70) {
+        return 'C'
+    } else if (score < 80) {
+        return 'B'
+    } else {
+        return 'A'
+    }
+}
+
+console.log(grade(40, 29, 29))
+
+
+
